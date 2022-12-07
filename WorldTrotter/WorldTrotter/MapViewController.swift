558b0c5bcde0efe585d8270b2f0badf0fe95d4b6
JeanCamargoReal <jeancamargoreal@icloud.com>
2022-12-07 14:54:54 -0300

    Create MapViewController

---
+++ WorldTrotter/WorldTrotter/MapViewController.swift
@@ -0,0 +1,13 @@
+//
+//  MapViewController.swift
+//  WorldTrotter
+//
+//  Created by Jean Camargo on 07/12/22.
+//
+
+import Foundation
+import UIKit
+
+class MapViewController: UIViewController {
+    
+}
