23342f894172da67bf6a0b1a06653b1b3f01abae
JeanCamargoReal <jeancamargoreal@icloud.com>
2022-12-08 14:52:53 -0300

    Updating the action

--- WorldTrotter/WorldTrotter/ConversionViewController.swift
+++ WorldTrotter/WorldTrotter/ConversionViewController.swift
@@ -9,10 +9,20 @@ import UIKit
 
 class ConversionViewController: UIViewController {
 
+    @IBOutlet var celsiusLabel: UILabel!
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
         print("ConversionViewController loaded its view.")
+    }
+
+    @IBAction func fahrenheitFieldEditingChanged(_ textField: UITextField) {
+        if let text = textField.text, !text.isEmpty {
+            celsiusLabel.text = text
+        } else {
+            celsiusLabel.text = "???"
+        }
     }
 }
 
