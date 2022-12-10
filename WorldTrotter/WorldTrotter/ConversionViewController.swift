fe89bdf25febe7f29a52538c5655f8173e42a265
JeanCamargoReal <jeancamargoreal@icloud.com>
2022-12-09 21:29:51 -0300

    Adding a property observer to fahrenheitValue

--- WorldTrotter/WorldTrotter/ConversionViewController.swift
+++ WorldTrotter/WorldTrotter/ConversionViewController.swift
@@ -12,7 +12,12 @@ class ConversionViewController: UIViewController {
     @IBOutlet var celsiusLabel: UILabel!
     @IBOutlet var textField: UITextField!
 
-    var fahrenheitValue: Measurement<UnitTemperature>?
+    var fahrenheitValue: Measurement<UnitTemperature>? {
+        didSet {
+            updateCelsiusLabel()
+        }
+    }
+
     var celsiusValue: Measurement<UnitTemperature>? {
         if let fahrenheitValue = fahrenheitValue {
             return fahrenheitValue.converted(to: .celsius)
@@ -25,6 +30,14 @@ class ConversionViewController: UIViewController {
         super.viewDidLoad()
 
         print("ConversionViewController loaded its view.")
+    }
+
+    func updateCelsiusLabel() {
+        if let celsiusValue = celsiusValue {
+            celsiusLabel.text = "\(celsiusValue.value)"
+        } else {
+            celsiusLabel.text = "???"
+        }
     }
 
     @IBAction func fahrenheitFieldEditingChanged(_ textField: UITextField) {
