a4ff1cde73264dad954ac0b761a3213df656d855
JeanCamargoReal <jeancamargoreal@icloud.com>
2023-01-07 12:36:10 -0300

    adding a button

--- Buggy/Buggy/ViewController.swift
+++ Buggy/Buggy/ViewController.swift
@@ -9,6 +9,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBAction func buttonTapped(_ sender: UIButton) {
+        print("Called buttonTapped(_:)")
+    }
+
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
