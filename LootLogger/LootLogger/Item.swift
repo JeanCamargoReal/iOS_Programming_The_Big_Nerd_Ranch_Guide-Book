dc17d81a50a4e5b51778447c5f0fe5734aa51e5b
JeanCamargoReal <jeancamargoreal@icloud.com>
2023-01-14 19:37:46 -0300

    Adding a convenience initializer

--- LootLogger/LootLogger/Item.swift
+++ LootLogger/LootLogger/Item.swift
@@ -9,14 +9,34 @@ import UIKit
 
 class Item {
     var name: String
-    var valueInDollar: Int
+    var valueInDollars: Int
     var serialNumber: String?
     let dateCreated: Date
     
-    init(name: String, valueInDollar: Int, serialNumber: String?) {
+    init(name: String, serialNumber: String?, valueInDollars: Int) {
         self.name = name
-        self.valueInDollar = valueInDollar
+        self.valueInDollars = valueInDollars
         self.serialNumber = serialNumber
         self.dateCreated = Date()
+    }
+    
+    convenience init(random: Bool = false) {
+        if random {
+            let adjectives = ["Fluffy", "Rusty", "Shiny"]
+            let nouns = ["Bear", "Spork", "Mac"]
+
+            let randomAdjective = adjectives.randomElement()!
+            let randomNoun = nouns.randomElement()!
+
+            let randomName = "\(randomAdjective) \(randomNoun)"
+            let randomValue = Int.random(in: 0..<100)
+            let randomSerialNumber = UUID().uuidString.components(separatedBy: "-").first!
+
+            self.init(name: randomName,
+                      serialNumber: randomSerialNumber,
+                      valueInDollars: randomValue)
+        } else {
+            self.init(name: "", serialNumber: nil, valueInDollars: 0)
+        }
     }
 }
