98c1a27e419395bb398f88fe7a6ec0acfe436778
JeanCamargoReal <jeancamargoreal@icloud.com>
2023-01-14 19:22:08 -0300

    Add LootLogger project

--- Buggy/Buggy/ViewController.swift
+++ Buggy/Buggy/ViewController.swift
@@ -10,15 +10,27 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBAction func buttonTapped(_ sender: UIButton) {
-        print("Called buttonTapped(_:)")
+        print("Method: \(#function) in file: \(#file) line: \(#line) called.")
+        
+        badMethod()
     }
-
+    
+    func badMethod() {
+        let array = NSMutableArray()
+        
+        for i in 0..<10 {
+            array.insert(i, at: i)
+        }
+        
+        // Go one step too far emptying the array (notice the range chante):
+        for _ in 0...10 {
+            array.removeObject(at: 0)
+        }
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
 }
 
---
+++ LootLogger/LootLogger/ItemsViewController.swift
@@ -0,0 +1,12 @@
+//
+//  ItemsViewController.swift
+//  LootLogger
+//
+//  Created by Jean Camargo on 14/01/23.
+//
+
+import UIKit
+
+class ItemsViewController: UITableViewController {
+    
+}
