9e55adef3851d0efb4b48b910084503563295bd9
JeanCamargoReal <jeancamargoreal@icloud.com>
2023-07-02 13:02:41 -0300

    Dequeuing ItemCell instances

--- LootLogger/LootLogger/Controller/ItemsViewController.swift
+++ LootLogger/LootLogger/Controller/ItemsViewController.swift
@@ -52,15 +52,17 @@ class ItemsViewController: UITableViewController {
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         // Get a new or recycled cell
-        let cell = tableView.dequeueReusableCell(withIdentifier: "UITableViewCell", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ItemCell", for: indexPath) as! ItemCell
 
         // Set the text on the cell with the description of the item
         // that is at the nth index of items, where n = row this cell
         // will appear in on table view
         let item  = itemStore.allItems[indexPath.row]
 
-        cell.textLabel?.text = item.name
-        cell.detailTextLabel?.text = "$\(item.valueInDollars)"
+		// Configure the cell with the item
+        cell.nameLabel.text = item.name
+		cell.serialNumberLabel.text = item.serialNumber
+		cell.valueLabel.text = "$\(item.valueInDollars)"
 
         return cell
     }
